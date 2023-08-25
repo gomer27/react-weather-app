15034091e6674d199a02362dd343ecad63580c5b
Ömer Güneş <omergunes2700@gmail.com>
2023-08-25 13:47:50 +0300

    havanın durumuna göre background değişiyor

--- src/City.jsx
+++ src/City.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 const City = (city) => {
   console.log(city);
+  if (!city.city) {
+    return <div>LÜTFEN DEĞER YAZINIZ...</div>;
+  }
   return (
-    <div className="background-image-container">
+    <div className={"background-image-container " + city.city.weather[0].main}>
       <div>
         <div className="space"></div>
 
@@ -11,10 +14,6 @@ const City = (city) => {
           <h1>{city.city.main.temp}</h1>
           <h1>{city.city.weather[0].main}</h1>
         </div>
-
-        {/* <h1>{city.city.main.temp}</h1>
-        <h1>{city.city.name}</h1>
-        <h1>{city.city.weather[0].main}</h1> */}
       </div>
     </div>
   );
